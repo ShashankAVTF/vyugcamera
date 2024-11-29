0a1ea12ef9cc8c7d8b0d0b2903a871a23fa15fe9
Shashank <Shashank@avtftechnology.com>
2024-11-29 11:10:14 +0530

    change to colgate test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
 
 (async function () {
   const cameraKit = await bootstrapCameraKit({
-    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMyNzc3OTIzLCJzdWIiOiI5YzRkNjMxNy1iYmI5LTRjODQtOWYzMC0wZTVmMzA3NDUxZGZ-U1RBR0lOR34zYjY4MTBkYy0xN2Y2LTRmNzctYTNiZS02YTdjMjU0Zjc2YjgifQ.La2HprjDjaVTHUAEgc7hvsk2xKx7krdqfvuPwdXzN9o',
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMyNzc3NDg1LCJzdWIiOiIwNDBlYThiNy0xMzk4LTRkOTktOThjMi1iZjUzN2ZmYmE5NDZ-U1RBR0lOR343NjEyMzgyOS0wYzQxLTQ2NWMtYjE3OC1iYjJlZWY4YTNlYTIifQ.GAm5xphhO0p9Jb5LESxzI9dOtTWwCA4lg78phW_Ks88',
   });
   const liveRenderTarget = document.getElementById(
     'canvas'
@@ -16,8 +16,8 @@ import { bootstrapCameraKit } from '@snap/camera-kit';
   await session.play();
 
   const lens = await cameraKit.lensRepository.loadLens(
-    'f0c77a23-a231-4206-9d7a-1008bc4c9629',
-    '67e7b4ff-7878-46fd-b778-adbf25e9a722'
+    'f5230eba-9ddd-43f1-95fd-941e37fef770',    //lens
+    '9a3cdccd-03ce-44cf-9939-a2f08a643ea1'     //group
   );
 
   await session.applyLens(lens);
